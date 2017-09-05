ec2dc21dc656697de4166286a6f26e5f05799427
Eugenie <zn@bk.ru>
2017-09-05 13:04:55 +1000

    Добавьте файлы проекта.

---
+++ Consolediff/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consolediff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
