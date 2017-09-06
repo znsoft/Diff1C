5497c540d622b164ebec348790e770eba0f51c2b
Eugenie <zn@bk.ru>
2017-09-06 17:01:43 +1000

    бета 1

--- Consolediff/Program.cs
+++ Consolediff/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Consolediff.filetree;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Consolediff
 {
     class Program
     {
+        [STAThread]
+
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ФормаВыбораФайлов());
         }
     }
 }
